ab522df6386409068e9abd4c9428ba7d0f55e231
khouloudhaddad <khouloud.haddad@gmail.com>
2023-05-05 21:30:59 +0100

    Pass variable to EJS

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.set('view-engine', 'ejs')
+
 app.get("/", (req, res)=>{
-    res.render('index.ejs')
+    res.render('index.ejs', {name: 'Joe'})
 })
 app.listen(3000)
