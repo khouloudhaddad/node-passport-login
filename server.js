7f703c86758d4701f6d9b729c839cb03b687a88c
khouloudhaddad <khouloud.haddad@gmail.com>
2023-05-06 00:21:44 +0100

    Register Post

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const path = require("path");
+const bcrypt = require('bcrypt')
 
 const app = express()
 
+const users = []
+
 app.use(express.static('public'));
+
+app.use(express.urlencoded({extended:false}))
 
 
 app.set('view-engine', 'ejs')
@@ -21,8 +26,26 @@ app.get("/register", (req, res)=>{
     res.render('register.ejs')
 })
 
-app.post("/register", (req, res)=>{
-    //res.render('register.ejs')
+app.post("/register", async (req, res)=>{
+   try{
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
+    users.push({
+        id: Date.now().toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+    });
+
+
+    res.redirect("/login")
+
+   }catch{
+    res.redirect("/register")
+   }
+
+   console.log(users)
 })
 
 
