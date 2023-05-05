55a072306516c6021a8c692280796270768f640a
khouloudhaddad <khouloud.haddad@gmail.com>
2023-05-05 23:10:59 +0100

    Add Custom Layout

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express')
+const path = require("path");
+
 const app = express()
 
+app.use(express.static('public'));
+
+
 app.set('view-engine', 'ejs')
+
 
 app.get("/", (req, res)=>{
     res.render('index.ejs', {name: 'Joe'})
 })
+
+app.get("/login", (req, res)=>{
+    res.render('login.ejs')
+})
+
+app.get("/register", (req, res)=>{
+    res.render('register.ejs')
+})
+
+
 app.listen(3000)
